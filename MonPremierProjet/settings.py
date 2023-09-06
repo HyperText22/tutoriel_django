f6509862087c909a4c22f5c3d8dd916143ebc84b
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-09-06 05:58:47 +0100

    Adaptation du code à la video 7

--- MonPremierProjet/settings.py
+++ MonPremierProjet/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MonPremierProjet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('all', views.get_all_post, name='post.all'),
+    path('post/<int:pk>', views.post, name='post.post'),
 ]
